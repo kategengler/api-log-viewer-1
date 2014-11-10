c1185895c0ea93f8baedaad43f2f5bccb5e8edca
Katie Gengler <katie@kmg.io>
2014-11-10 14:36:09 -0500

    Fix CSP

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
   }
 
   ENV.contentSecurityPolicy = {
-    'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
+    'font-src': "'self' https://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
     'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
   }
 
