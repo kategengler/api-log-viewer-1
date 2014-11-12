66a8f161a7ec4ec1f330f5eec9bc418d98d806e0
Katie Gengler <katie@kmg.io>
2014-11-12 14:07:40 -0500

    Filter by dates

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    filter: function(emailFilter){
-      this.transitionTo('events', {queryParams: {email: emailFilter}});
+    filter: function(filters){
+      this.transitionTo('events', {queryParams: filters});
     }
   }
 });
